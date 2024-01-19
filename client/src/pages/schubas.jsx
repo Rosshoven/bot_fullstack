f573e5f8943b813eacc2bd871a23a901683edf2e
Rosshoven <Michaeledwardross@gmail.com>
2024-01-19 15:34:01 -0600

    Adding tracks

--- client/src/pages/schubas.jsx
+++ client/src/pages/schubas.jsx
@@ -1,6 +1,7 @@
 import Album from '../components/album';
-import SchubasBack from '../images/SchubasBack.png'
-import Schubas_cover from '../images/Schubas_cover.JPG'
+import SchubasBack from '../images/SchubasBack.png';
+import Schubas_cover from '../images/Schubas_cover.JPG';
+import 
 
 export default function Schubas() {
 
@@ -9,17 +10,17 @@ export default function Schubas() {
     <div style={{backgroundColor: 'pink', padding: '.1rem'}}>
         <Album 
                image={Schubas_cover} 
-               description={`If it's the last thing we do Album Cover`} 
-               songTitle1={`Rex`}
-               songTitle2={`Fireplay`}
-               songTitle3={`Look Left`}
-               songTitle4={`Say Something`}
-               songTitle5={`Get In Line`}
-               // track1={}
-               // track2={}
-               // track3={}
-               // track4={}
-               // track5={}
+               description={`Live at Schuba's Album Cover`} 
+               songTitle1={`Hey!`}
+               songTitle2={`Sugar Cereal`}
+               songTitle3={`Blurry Faces`}
+               songTitle4={`The Addict`}
+               songTitle5={`Run (from the city)`}
+               track1={}
+               track2={}
+               track3={}
+               track4={}
+               track5={}
                pageBackgroundColor={'lightyellow'}
                backgroundColor={'skyblue'}
                border={'black 2px solid'}
